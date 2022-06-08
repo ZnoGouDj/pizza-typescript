07d470f3e63d858b0478f385b22e7c7ad5308b99
ZnoGouDj <znogoud3@gmail.com>
2022-06-08 23:07:09 +0200

    Skeleton

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React from 'react';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
-
-import pizzas from './components/pizzas.json';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+import Skeleton from './components/PizzaBlock/Skeleton';
 
 import './scss/app.scss';
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch('https://62a0f78a7b9345bcbe4358a7.mockapi.io/items')
       .then(res => res.json())
       .then(arr => {
         setItems(arr);
+        setIsLoading(false);
       });
   }, []);
 
@@ -30,9 +31,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map(({ ...props }) => (
-              <PizzaBlock key={props.title} {...props} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : items.map(({ ...props }) => <PizzaBlock key={props.title} {...props} />)}
           </div>
         </div>
       </div>
