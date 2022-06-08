8815e52c5bba40c2bed04fdb33a89bf6b4ef72c5
ZnoGouDj <znogoud3@gmail.com>
2022-06-08 23:21:02 +0200

    Add pages, export context to Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://62a0f78a7b9345bcbe4358a7.mockapi.io/items')
+      .then(res => res.json())
+      .then(arr => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map(({ ...props }) => <PizzaBlock key={props.title} {...props} />)}
+      </div>
+    </>
+  );
+};
+
+export default Home;
